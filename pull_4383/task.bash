3481ef2bea57a69bfa1cd28550852c8bff26ebf1
Alexey Melezhik <melezhik@gmail.com>
2021-06-22 18:44:26 +0000

    right test

--- pull_4383/task.bash
+++ pull_4383/task.bash
@@ -2,4 +2,10 @@ cat $root_dir/task.bash
 
 $RAKUBIN -v
 
-time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; for (1..10) { my (@m) = $txt.grep( { m:i/ <!after i> e / } ) }'
+echo .grep 
+time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; for (1..10) { my (@m) = $txt.grep( { m:i/ <!after i> e / } ) }' 2>&1
+echo end
+
+echo "grep .contains"
+time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; for (1..10) { my (@m) = $txt.grep( *.contains( /:i <!after i> e / ) ) }' 2>&1
+echo end
