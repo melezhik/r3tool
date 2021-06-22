9c5fbbb09615a0b8841fb8f2d91abaddf84bcb08
Alexey Melezhik <melezhik@gmail.com>
2021-06-22 21:28:04 +0000

    use Raku for timeing

--- pull_4383/task.bash
+++ pull_4383/task.bash
@@ -2,5 +2,5 @@ cat $root_dir/task.bash
 
 $RAKUBIN -v
 
-time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; $txt.grep( /:i <!after i> e / ) for 1..1000' 
+$RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; $txt.grep( /:i <!after i> e / ) for 1..1000; say now - INIT now' 
 
