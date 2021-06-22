e3430928d376d3238b5441c874f65269a23859ea
Alexey Melezhik <melezhik@gmail.com>
2021-06-22 20:59:13 +0000

    anotrher benchmark for https://github.com/rakudo/rakudo/pull/4383

---
+++ .tomty/env/config.2021_05.pl6
@@ -0,0 +1,3 @@
+%*ENV<RAKUBIN> = "/tmp/whateverable/rakudo-moar/3bdab142d27e8e60f771f82dfc690b69b39638bb/bin/raku";
+
+%();
--- pull_4383/task.bash
+++ pull_4383/task.bash
@@ -2,10 +2,5 @@ cat $root_dir/task.bash
 
 $RAKUBIN -v
 
-echo .grep 
-time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; for (1..10) { my (@m) = $txt.grep( { m:i/ <!after i> e / } ) }' 2>&1
-echo end
+time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; $txt.grep( /:i <!after i> e / ) for 1..1000' 
 
-echo "grep .contains"
-time $RAKUBIN -e 'my $txt = slurp "/usr/share/dict/words"; for (1..10) { my (@m) = $txt.grep( *.contains( /:i <!after i> e / ) ) }' 2>&1
-echo end
