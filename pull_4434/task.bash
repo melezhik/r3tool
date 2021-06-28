579f2a455a6616a43bf9d2bc2e9fbd1469743abd
Alexey Melezhik <melezhik@gmail.com>
2021-06-28 22:52:15 +0000

    add another case

--- pull_4434/task.bash
+++ pull_4434/task.bash
@@ -17,3 +17,5 @@ $RAKUBIN -e 'say "ex6 ", modf(123456789123456789.6,2).perl'
 $RAKUBIN -e 'say "ex7 ", modf(+6.2,2).perl'
 
 $RAKUBIN -e 'say "ex8 ", modf(+6.2,100).perl'
+
+$RAKUBIN -e 'say "ex9 ", modf(+6.2,4).perl'
