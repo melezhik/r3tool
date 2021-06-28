be7314b307c01cc13764bd06eab03994f264d08c
Alexey Melezhik <melezhik@gmail.com>
2021-06-28 23:05:57 +0000

    add more check

--- make_dollar_work/task.bash
+++ make_dollar_work/task.bash
@@ -6,7 +6,7 @@ cat << 'HERE' > $cache_root_dir/code.raku
     my \a = $(42);
     say a;  # 42
     a = 666;
-    say a;  # 666
+    say "[", a, "]";  # 666
 HERE
 
 $RAKUBIN $cache_root_dir/code.raku
