497b2fd434db61d8dd6d66f296405a8bd796009e
Alexey Melezhik <melezhik@gmail.com>
2021-06-28 22:45:45 +0000

    r3 test for https://github.com/rakudo/rakudo/pull/4434

---
+++ .tomty/pull_4434.pl6
@@ -0,0 +1,11 @@
+
+#!raku
+
+=begin tomty
+%(
+  tag => $["pr", "math", "fresh", "open", "fr"]
+);
+=end tomty
+
+task-run "pull_4434";
+
---
+++ pull_4434/task.bash
@@ -0,0 +1,19 @@
+cat $root_dir/task.bash
+
+$RAKUBIN -v
+
+$RAKUBIN -e 'say "ex1 ", modf(0.0).perl'
+
+$RAKUBIN -e 'say "ex2 ", modf(-0.0).perl'
+
+$RAKUBIN -e 'say "ex3 ", modf(0).perl'
+
+$RAKUBIN -e 'say "ex4 ", modf(.6).perl'
+
+$RAKUBIN -e 'say "ex5 ", modf(.6,2).perl'
+
+$RAKUBIN -e 'say "ex6 ", modf(123456789123456789.6,2).perl'
+
+$RAKUBIN -e 'say "ex7 ", modf(+6.2,2).perl'
+
+$RAKUBIN -e 'say "ex8 ", modf(+6.2,100).perl'
