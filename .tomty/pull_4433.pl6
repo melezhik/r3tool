2c4435bc66db4465c6bb310e7444869749848f6a
Alexey Melezhik <melezhik@gmail.com>
2021-06-29 17:07:24 +0000

    r3 test for https://github.com/rakudo/rakudo/pull/4433

---
+++ .tomty/pull_4433.pl6
@@ -0,0 +1,11 @@
+
+#!raku
+
+=begin tomty
+%(
+  tag => $["pod", "open", "rc_2021_07"]
+);
+=end tomty
+
+task-run "pull_4433";
+
---
+++ pull_4433/task.bash
@@ -0,0 +1,36 @@
+cat $root_dir/task.bash
+
+cat << 'HERE' > $cache_dir/test1.pod
+=for pod
+=for nested
+=for para :nested(1)
+E<alpha;beta>E<alpha;beta;gamma>
+♥♥♥
+HERE
+
+echo "ex1"
+echo "==="
+$RAKUBIN --doc=Text $cache_dir/test1.pod
+echo "==="
+
+cat << 'HERE' > $cache_dir/test2.pod
+=begin nested
+A
+=begin nested
+B
+=begin nested
+C
+=begin nested
+D
+=end nested
+=end nested
+=end nested
+=end nested
+HERE
+
+
+echo "ex2"
+echo "==="
+$RAKUBIN --doc=Text $cache_dir/test2.pod
+echo "==="
+
