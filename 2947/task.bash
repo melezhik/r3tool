b86322ebb059bc5d89abb8c1b7cf0fc33aeae9db
Alexey Melezhik <melezhik@gmail.com>
2021-07-06 18:39:14 +0000

    correct test for https://github.com/rakudo/rakudo/issues/2947

--- 2947/task.bash
+++ 2947/task.bash
@@ -2,22 +2,10 @@ cat $root_dir/task.bash
 
 $RAKUBIN -v
 
-echo "repl >>>"
-
-cat  << 'HERE' | $RAKUBIN --repl-mode=interactive
+cat  << 'HERE' | $RAKUBIN --repl-mode=interactive 2>&1
 my $p = start { say 1 / 0 };
 sink $p;
 say 'done';
 HERE
 
-echo "<<<"
 
-echo "as_script >>>"
-
-cat  << 'HERE' | $RAKUBIN --repl-mode=non-interactive
-my $p = start { say 1 / 0 };
-sink $p;
-say 'done';
-HERE
-
-echo "<<<"
