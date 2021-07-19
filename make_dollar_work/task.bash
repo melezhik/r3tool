44f70113e3472bebd2665bfc03c73a21c1c1ac5d
Alexey Melezhik <melezhik@gmail.com>
2021-07-19 15:58:32 +0000

    use v6.e.PREVIEW for some tests

--- make_dollar_work/task.bash
+++ make_dollar_work/task.bash
@@ -3,6 +3,7 @@ cat $root_dir/task.bash
 $RAKUBIN -v
 
 cat << 'HERE' > $cache_root_dir/code.raku
+    use v6.e.PREVIEW;
     my \a = $(42);
     say a;  # 42
     a = 666;
--- pull_4415/task.bash
+++ pull_4415/task.bash
@@ -7,31 +7,31 @@ $RAKUBIN -v
 echo 'ex1: say ^10 .map: { $_ == 5 ?? (last)   !! $_ }'
 $RAKUBIN -e 'say ^10 .map: { $_ == 5 ?? (last)   !! $_ }' 2>&1 # (0 1 2 3 4)
 
-echo 'ex2: say ^10 .map: { $_ == 5 ?? last(42) !! $_ }'
-$RAKUBIN -e 'say ^10 .map: { $_ == 5 ?? last(42) !! $_ }' 2>&1 # (0 1 2 3 4 42)
+echo 'ex2: use v6.e.PREVIEW; say ^10 .map: { $_ == 5 ?? last(42) !! $_ }'
+$RAKUBIN -e 'use v6.e.PREVIEW; say ^10 .map: { $_ == 5 ?? last(42) !! $_ }' 2>&1 # (0 1 2 3 4 42)
 
 # and also in .grep:
 
-echo 'ex3.1: say ^10 .grep: { $_ == 5 ?? (last)       !! $_ %% 2 }'
-$RAKUBIN -e 'say ^10 .grep: { $_ == 5 ?? (last)       !! $_ %% 2 }' 2>&1 # (0 2 4)
+echo 'ex3.1: use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? (last)       !! $_ %% 2 }'
+$RAKUBIN -e 'use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? (last)       !! $_ %% 2 }' 2>&1 # (0 2 4)
 
-echo 'ex3.2: say ^10 .grep: { $_ == 5 ?? last()       !! $_ %% 2 }'
-$RAKUBIN -e 'say ^10 .grep: { $_ == 5 ?? last()       !! $_ %% 2 }' 2>&1 # (0 2 4)
+echo 'ex3.2: use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? last()       !! $_ %% 2 }'
+$RAKUBIN -e 'use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? last()       !! $_ %% 2 }' 2>&1 # (0 2 4)
 
-echo 'ex4: say ^10 .grep: { $_ == 5 ?? last(True) !! $_ %% 2 }'
-$RAKUBIN -e 'say ^10 .grep: { $_ == 5 ?? last(True) !! $_ %% 2 }' 2>&1 # (0 2 4 5)
+echo 'ex4: use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? last(True) !! $_ %% 2 }'
+$RAKUBIN -e 'use v6.e.PREVIEW; say ^10 .grep: { $_ == 5 ?? last(True) !! $_ %% 2 }' 2>&1 # (0 2 4 5)
 
 # and any other loop constructs that return values, e.g.:
 
-echo 'ex5: my $i; say do while ++$i < 10 { $i == 5 ?? (last)   !! $i }'
-$RAKUBIN -e 'my $i; say do while ++$i < 10 { $i == 5 ?? (last)   !! $i }' 2>&1 # (1 2 3 4)
+echo 'ex5: use v6.e.PREVIEW; my $i; say do while ++$i < 10 { $i == 5 ?? (last)   !! $i }'
+$RAKUBIN -e 'use v6.e.PREVIEW; my $i; say do while ++$i < 10 { $i == 5 ?? (last)   !! $i }' 2>&1 # (1 2 3 4)
 
 # vs
 
-echo 'ex6: my $i; say do while ++$i < 10 { $i == 5 ?? last(42) !! $i }'
-$RAKUBIN -e 'my $i; say do while ++$i < 10 { $i == 5 ?? last(42) !! $i }' 2>&1  # (1 2 3 4 42)
+echo 'ex6: use v6.e.PREVIEW; my $i; say do while ++$i < 10 { $i == 5 ?? last(42) !! $i }'
+$RAKUBIN -e 'use v6.e.PREVIEW; my $i; say do while ++$i < 10 { $i == 5 ?? last(42) !! $i }' 2>&1  # (1 2 3 4 42)
 
-echo 'ex7: dd ^10 .map: { last slip(42,66) if $_ == 5; $_ }'
-$RAKUBIN -e 'dd ^10 .map: { last slip(42,66) if $_ == 5; $_ }' 2>&1
+echo 'ex7: use v6.e.PREVIEW; dd ^10 .map: { last slip(42,66) if $_ == 5; $_ }'
+$RAKUBIN -e 'use v6.e.PREVIEW; dd ^10 .map: { last slip(42,66) if $_ == 5; $_ }' 2>&1
 
 echo
