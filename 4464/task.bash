ded36aa3ef9038053f4af865b6007e1da5327421
Alexey Melezhik <melezhik@gmail.com>
2021-07-21 02:18:16 +0000

    r3 for https://github.com/rakudo/rakudo/issues/4464

---
+++ 4464/task.bash
@@ -0,0 +1,13 @@
+cat $root_dir/task.bash
+
+$RAKUBIN -v
+
+$RAKUBIN -e 'print "ex1: "; say Uni(97) cmp Uni(97)' 2>&1
+
+$RAKUBIN -e 'print "ex2: "; say Uni(97) ~~ Uni(97)' 2>&1
+
+$RAKUBIN -e 'print "ex3: "; my int @a = 1,2,3; my @b = 1,2,3; dd @a cmp @b' 2>&1
+
+# ----------------------------------------------------------------
+
+echo
