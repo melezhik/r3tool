f06e3ef5c6b5c5a3ac2c46d8f1b4c9a399b645b7
Alexey Melezhik <melezhik@gmail.com>
2022-06-09 11:07:39 -0500

    test for https://github.com/rakudo/rakudo/issues/4571

---
+++ 4571/task.bash
@@ -0,0 +1,27 @@
+cat $root_dir/task.bash
+
+$RAKUBIN -v
+
+cat << 'HERE' > $cache_root_dir/code.raku
+
+class ASNType {
+    has $.type is rw; # NO BUG IF REMOVE `is rw`
+}
+
+my $new-type = Metamodel::ClassHOW.new_type(name => 'LDAPMessage');
+
+my $attribute-type = ASNType.new(type => Int);
+
+my $attr = Attribute.new(name => '$!protocol-op', type => $attribute-type.type, package => $new-type, :has_accessor); # NO BUG IF REMOVE `has_accessor`
+$new-type.^add_attribute($attr);
+
+$new-type.^compose;
+
+say $new-type.new; # expected -> type, real:
+
+# Invocant of method 'raku' must be an object instance of type 'Mu', not
+# a type object of type 'ProtocolOp'.  Did you forget a '.new'?
+
+HERE
+
+$RAKUBIN $cache_root_dir/code.raku
