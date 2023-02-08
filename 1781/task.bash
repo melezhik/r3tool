7ced0d7bde37a7e394ce6eca3c08bdde17a732b7
Alexey Melezhik <melezhik@gmail.com>
2023-02-07 21:11:41 -0600

    fix 1781 test

--- 1781/task.bash
+++ 1781/task.bash
@@ -3,12 +3,25 @@ set -e
 $RAKUBIN -v
 
 cat << 'HERE' > $cache_dir/code.raku
-multi sub a() { 
-  dd callframe(2).my
-};
-a;
-a;
-a
+
+{
+     my $seen;
+     multi sub a() { ++$seen if callframe(1).my.EXISTS-KEY(<$seen>) };
+
+     # call a 300 times
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;a;
+     say "[seen=$seen]";
+ }
+
 HERE
 
 $RAKUBIN $cache_dir/code.raku 2>&1
