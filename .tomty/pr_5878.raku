f42f1a6b2e33aa3884a8c53ef4088a6b09212bdf
Alexey Melezhik <melezhik@gmail.com>
2025-05-12 22:18:35 +0300

    https://github.com/rakudo/rakudo/pull/5878

---
+++ .tomty/pr_5878.raku
@@ -0,0 +1,11 @@
+
+#!raku
+
+=begin tomty
+%(
+  tag => $["pr", "closed"]
+);
+=end tomty
+
+task-run "pr_5878";
+
---
+++ pr_5878/task.bash
@@ -0,0 +1,10 @@
+set -e
+
+$RAKUBIN -v
+
+cat  << 'HERE' | $RAKUBIN --repl-mode=interactive
+module M { enum E is export <F G> }
+import M;
+sub MAIN (E $f, E :$g) { ... }
+HERE
+
