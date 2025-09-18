db90d6c703df78ae02b7f24dd7e6f54e2c88b09e
Alexey Melezhik <melezhik@gmail.com>
2025-09-18 12:43:27 +0300

    fix tests

--- pr_5878/task.bash
+++ pr_5878/task.bash
@@ -5,6 +5,7 @@ $RAKUBIN -v
 cat  << 'HERE' | $RAKUBIN --repl-mode=interactive
 module M { enum E is export <F G> }
 import M;
-sub MAIN (E $f, E :$g) { ... }
+sub MAIN (E $f, E :$g) { ... };
+exit;
 HERE
 
